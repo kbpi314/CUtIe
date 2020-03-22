c706d157afdd825ede465b78930db19fda34a4dc
Kevin Bu <kbpi314@gmail.com>
2020-03-22 18:08:03 -0400

    added missing ;

--- scripts/analyze_simulations.py
+++ scripts/analyze_simulations.py
@@ -212,7 +212,7 @@ def analyze_simulations(fold_value, statistic, multi_corr, corr_compare, classes
 
     corr_ticks = [0, 0.05, 0.10, 0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45, 0.50, 0.55, 0.60, 0.65, 0.70, 0.75, 0.80, 0.85, 0.90, 0.95]
     # indiv plots
-    for p in param.split(',')
+    for p in param.split(','):
         for mc in multi_corr.split(','):
             for fv in fold_value.split(','):
                 for stat in stat_pairs:
